48ba17f574b5ff0bf44782e41902da636eeef130
bgpat <bgpat@bgpat.net>
2015-12-25 05:04:20 +0900

    Generate ramdom padding

--- lib/dns/requests/fields/paddig.py
+++ lib/dns/requests/fields/paddig.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
+import random
 from field import RequestField
 
-class Padding(RequestField)
+class Padding(RequestField):
+    RANDOM_STRING = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_'
     name = 'padding'
     default = 0
+    separate = 63
     def __str__(self):
-        self.value
-
-
-
+        arr = [random.choice(self.RANDOM_STRING) for x in xrange(self.separate)]
+        res = '.'.join([''.join(arr) for y in xrange(self.value)])
+        res = res[:self.value]
+        if res[-1:] == '.':
+            res = res[:-1] + random.choice(self.RANDOM_STRING)
+        return res
